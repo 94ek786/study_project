4f47bf8407477105ffd13556525a3dddf25e39f0
94ek786 <e0970633123@gmail.com>
2022-12-05 17:05:03 +0800

    Update _add_keep.php

--- _add_keep.php
+++ _add_keep.php
@@ -5,20 +5,21 @@ $conn=require_once "config.php";
 if($_SERVER["REQUEST_METHOD"] != "POST"){
     header('location:login.php');
 }
-if($GLOBALS['username'] == ){
-    header('location:login.php');
-}
-$check="SELECT * FROM collect WHERE user='".$GLOBALS['username']."' and house='".$_POST['ID']."'";
-$result = mysqli_query($conn,$check);
-if(mysqli_num_rows($result)==0){
-    $sql="INSERT INTO collect (user, house) VALUES('".$GLOBALS['username']."','".$_POST['ID']."')";
-    if(mysqli_query($conn, $sql)){
-        echo '<script>alert("收藏成功");history.back();</script>';
-        exit;
+if($GLOBALS['username'] != 'none'){
+    $check="SELECT * FROM collect WHERE user='".$GLOBALS['username']."' and house='".$_POST['ID']."'";
+    $result = mysqli_query($conn,$check);
+    if(mysqli_num_rows($result)==0){
+        $sql="INSERT INTO collect (user, house) VALUES('".$GLOBALS['username']."','".$_POST['ID']."')";
+        if(mysqli_query($conn, $sql)){
+            echo '<script>alert("收藏成功");history.back();</script>';
+            exit;
+        }else{
+            echo "伺服器似乎出問題了 :" . mysqli_error($conn);
+        }
     }else{
-        echo "伺服器似乎出問題了 :" . mysqli_error($conn);
+        echo '<script>alert("已收藏過");history.back();</script>';
     }
 }else{
-    echo '<script>alert("已收藏過");history.back();</script>';
+    header('location:login.php');
 }
 ?>
